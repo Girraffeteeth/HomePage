81365ed8a428a6b0b10dca3eec5c15c479d76e0b
Meghna Das <gdyes128@gmail.com>
2023-11-09 21:29:46 +0530

    changes made

---
+++ library-management-system/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import HomePage from './components/HomePage';
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
